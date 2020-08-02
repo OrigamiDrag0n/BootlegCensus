11c17e4d8a1c420ac73a85de25a4a671ef63c56e
DragonFolder <48910195+OrigamiDrag0n@users.noreply.github.com>
2020-08-02 11:00:41 +0100

    Main files upload

---
+++ bootleg_censor_1.py
@@ -0,0 +1,42 @@
+##WordCloud of bootleg performances, just out of curiosity
+
+import requests
+from bs4 import BeautifulSoup
+from wordcloud import WordCloud
+from matplotlib import pyplot as plt
+
+URL = 'https://livebootlegconcert.blogspot.com/'
+
+def import_artists():
+
+    response = requests.get(URL)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        sidebar = BeautifulSoup(soup.find('aside').prettify(), features='lxml')
+        bands = sidebar.findAll('a', {'dir': 'ltr'})
+        freqs = sidebar.findAll('span', {'dir': 'ltr'})
+        freq_dict = {}
+        for band, freq in zip(bands, freqs):
+            band_text = band.text.strip()
+            if not (band_text[0] == '_' or band_text[:8] == 'Festival' or band_text == None):
+                freq_int = int(freq.text.strip()[1:-1])
+                freq_dict[band_text] = freq_int    
+        return freq_dict
+
+def color_func(dictionary):
+
+    maximum = max(dictionary.values())
+    def color_func_inner(word, font_size, position, orientation, random_state=None, **kwargs):
+        return 'hsl(%d, 80%%, 50%%)' % (360 * dictionary[word]/maximum)
+    return color_func_inner
+
+if __name__ == '__main__':
+
+    freq_dict = import_artists()
+    print(sum(freq_dict.values()))
+    wordcloud = WordCloud(background_color = 'black', width = 2000, height = 1400, color_func = color_func(freq_dict)).generate_from_frequencies(freq_dict)
+    plt.imshow(wordcloud, interpolation = 'bilinear')
+    plt.axis('off')
+    plt.tight_layout()
+    plt.savefig('.\wordcloud.png', dpi = 300)
+    plt.show()
