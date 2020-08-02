5633f045d03fcb47797ce826032a51495fb547ce
DragonFolder <48910195+OrigamiDrag0n@users.noreply.github.com>
2020-08-02 12:03:53 +0100

    Adding disclaimer

--- census.py
+++ census.py
@@ -1,4 +1,10 @@
 ##WordCloud of bootleg performances, just out of curiosity
+##The bootlegging site can be found at https://livebootlegconcert.blogspot.com/
+##DISCLAIMER:
+##This code does not under any conditions endorse bootlegging, as it is illegal
+##and threatens the integrity of the music industry, which is already struggling
+##as a result of lockdown and defunding of the arts. Please use this code with
+##caution, as this brief project was purely made out of curiosity.
 
 import requests
 from bs4 import BeautifulSoup
